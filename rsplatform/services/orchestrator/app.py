814a0e7a8316b92d6934e1bed7674d6650500f15
p.phahsafi <p.phahsafi@pantherx.org>
2019-02-02 13:21:37 +0330

    orchestrator is running

--- rsplatform/services/orchestrator/app.py
+++ rsplatform/services/orchestrator/app.py
@@ -1,1 +1,9 @@
-print("hello")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hey, we have Flask in a Docker container!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
