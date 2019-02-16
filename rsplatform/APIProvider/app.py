193fc439c72daf2e66a10cce2a8475bf4817f290
pd <pd.shahsafi@gmail.com>
2019-02-16 13:52:44 +0330

    data base configed and connect to flask(pd)

--- rsplatform/APIProvider/app.py
+++ rsplatform/APIProvider/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from pymongo import MongoClient
+import os
+
+db_client = MongoClient(host="0.0.0.0", port=27017)
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+    return "service is running..."
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
