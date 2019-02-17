3348f27eab4247621f0fef3c9215f3bb3e74db7e
p.phahsafi <p.phahsafi@pantherx.org>
2019-02-17 15:52:52 +0330

    API starts

--- rsplatform/APIProvider/app.py
+++ rsplatform/APIProvider/app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from pymongo import MongoClient
 import os
+from resources.user import get
 
 db_client = MongoClient(host="0.0.0.0", port=27017)
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def hello_world():
-    return "service is running..."
+    return get()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
