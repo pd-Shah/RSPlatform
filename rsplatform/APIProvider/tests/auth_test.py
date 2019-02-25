81c387f878bc87f9db00ae095d342ff5c805c53d
p.phahsafi <p.phahsafi@pantherx.org>
2019-02-25 15:36:10 +0330

    testing 0.0.0.1

--- rsplatform/APIProvider/tests/auth_test.py
+++ rsplatform/APIProvider/tests/auth_test.py
@@ -2,7 +2,7 @@ import requests
 import unittest
 
 def url(endpoint, url="http://0.0.0.0:5000"):
-    url=os.popen(("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' rsplatform_apiprovider_1")).read().split("\n")
+    url=os.popen(("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' rsplatform_apiprovider_1")).read().split("\n")[0]
     url+=":5000"
     print(url)
     return (url+endpoint).strip()
