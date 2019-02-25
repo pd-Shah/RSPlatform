67b4dc3e391a7118e187cc20c249f72dc544c614
p.phahsafi <p.phahsafi@pantherx.org>
2019-02-25 14:26:10 +0330

    +100 herioc commit. pd(c)

--- rsplatform/APIProvider/tests/auth_test.py
+++ rsplatform/APIProvider/tests/auth_test.py
@@ -5,7 +5,7 @@ def url(endpoint, url="http://127.0.0.1:5000"):
     return (url+endpoint).strip()
 
 class AuthTest(unittest.TestCase):
-    def test_connection(self, endpoint="/auth/register"):
+    def test_connection(self, endpoint="/api/v1/register"):
         response = None
         response = requests.get(url(endpoint))
         self.assertFalse(response is None, msg="500 Internal Server Error.")
